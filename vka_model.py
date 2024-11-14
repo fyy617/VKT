61d1f582c7ff11244739229e00eb0b180ca48651
VKA <3308600758@qq.com>
2024-11-14 10:19:10 +0800

    Update vka_model.py

--- vka_model.py
+++ vka_model.py
@@ -9,16 +9,6 @@ import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 from einops import rearrange, repeat
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
-try:
-    from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, selective_scan_ref
-except:
-    pass
-
-try:
-    from selective_scan import selective_scan_fn as selective_scan_fn_v1
-    from selective_scan import selective_scan_ref as selective_scan_ref_v1
-except:
-    pass
 
 DropPath.__repr__ = lambda self: f"timm.DropPath({self.drop_prob})"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
